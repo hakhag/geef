5b39d11c4fc5a96f36d9c574c9f43beaadca2549
Håkon Hagen <hakonhagen2@gmail.com>
2014-05-19 22:31:56 +0200

    Fixed the scrubbing

--- js/jquery.geef.js
+++ js/jquery.geef.js
@@ -8,6 +8,8 @@
 		settings = $.extend({}, defaults, options);
 
 		return this.each(function() {
+			appendDOM(this);
+
 			var img = $(this).find('img.geef-scrubber'),
 				timeline = $(this).find('.timeline');
 
@@ -16,34 +18,37 @@
 				interval: null,
 				scrubber: img,
 				timeline: timeline,
-				tile: {				
+				frame: {				
 					width: img.outerWidth(),
 					height: img.outerHeight() / (img.outerHeight() / $(this).innerHeight())
 				},
 			};
 
-			this.attrs.noTiles = this.attrs.scrubber.outerHeight() / this.attrs.tile.height;
-			this.attrs.spacing = this.attrs.tile.width / this.attrs.noTiles;
+			this.attrs.framesCount = (this.attrs.scrubber.outerHeight() / this.attrs.frame.height)-1;
+			this.attrs.spacing = this.attrs.frame.width / this.attrs.framesCount;
 
-			startAnimation(this);
-			appendPoints(this);
+			// startAnimation(this);
+			// appendPoints(this);
 
 			$(this).mouseenter(function() {
 				stopAnimation(this);
 			}).mousemove(function(e){
-				scrubToPoint(e, this);
+				scrubToFrame(e, this);
 			}).mouseleave(function(){
 				startAnimation(this);
 			});
 		});
 
+		function appendDOM(geef) {
+			$(geef).append('<div class="timeline"></div>');
+		}
+
 		function startAnimation(geef) {
-			console.log('Starting animation ..');
 			var attrs = geef.attrs;
 			attrs.interval = setInterval(function(){
 				var perc = (Math.abs(attrs.t) / attrs.scrubber.outerHeight()) * 100;
 				attrs.scrubber.css('top', attrs.t + 'px');
-				attrs.t -= attrs.tile.height;
+				attrs.t -= attrs.frame.height;
 				animateTimeline(geef, perc);
 				if (attrs.t <= -attrs.scrubber.outerHeight()) {
 					attrs.t = 0;
@@ -52,29 +57,26 @@
 		}
 
 		function appendPoints(geef) { 
-			for(var i = 0; i < geef.attrs.noTiles; i++) {
+			for(var i = 0; i < geef.attrs.framesCount; i++) {
 				$(geef).append('<div class="line" style="width:'+ geef.attrs.spacing +'px; left:'+ i*geef.attrs.spacing +'px"></div>');
 			}
 		} 
 
 		function stopAnimation(geef) {
-			console.log('Stopping ...');
 			clearInterval(geef.attrs.interval);
+			geef.attrs.interval = null;
 		}
 
-		function scrubToPoint(e, geef) {
-			console.log('Scrubbing ..');
+		function scrubToFrame(e, geef) {
 			var posX = e.pageX - $(geef).offset().left,
-				percentagePosX = (posX * 100) / geef.attrs.tile.width;
-			// console.log(posX + ', ' + percentagePosX + '%');
+				percentagePosX = (posX * 100) / geef.attrs.frame.width,	
+				activeFrame = Math.ceil((posX / geef.attrs.spacing));
 
-			var scrubTo = -Math.abs(geef.attrs.scrubber.outerHeight() * (percentagePosX / 100));
-
-			// console.log(scrubTo);
-
-			geef.attrs.t = scrubTo;
-			geef.attrs.scrubber.css('top', geef.attrs.t + 'px');
-			animateTimeline(geef, percentagePosX);
+			if(activeFrame <= geef.attrs.framesCount) {
+				geef.attrs.t = -Math.abs(activeFrame * geef.attrs.frame.height);
+				geef.attrs.scrubber.css('top', geef.attrs.t + 'px');
+				animateTimeline(geef, percentagePosX);	
+			}
 		}
 
 		function animateTimeline(geef, width) {
