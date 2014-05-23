0e78e5e95129fe6cc2f6438e8e3d212adaf22bd0
hakhag <hakonhagen2@gmail.com>
2014-05-23 15:44:34 +0200

    Major changes, started on responsive

--- js/jquery.geef.js
+++ js/jquery.geef.js
@@ -3,30 +3,62 @@
 	$.fn.geef = function(options){
 		var defaults = {
 			speed: 45,
+			responsive: true
 		},
-
 
 		settings = $.extend({}, defaults, options);
 
 		return this.each(function() {
+			var _this = this;
 
 			// Append timeline and control
 			appendDOM(this);
 
-			// Get stuff
-			var _this = this,
-				scrubber = $(this).find('img.geef-scrubber'),
-				scrubberHeight =  scrubber.innerHeight(),
-				frameHeight = scrubber.innerHeight() / (scrubber.innerHeight() / $(this).innerHeight()),
+			// Sets the height of .geef-wrapper
+			// Caveat: it assumes frame is 16:9
+			// $(this).css('height', setHeight(this.getBoundingClientRect().width) + 'px');
+			$(this).css('height', Math.floor(setHeight($(this).innerWidth())) + 'px');
+
+			// Builds object properties
+			this.attrs = init(this);
+			// console.log(this.attrs);
+
+			// Bind window resize, perhaps not needed
+			if(settings.responsive) {
+				$(window).on('resize', function(){
+					$(_this).css('height', setHeight(_this.getBoundingClientRect().width) + 'px');
+					_this.attrs = init(_this);
+				});
+			}
+
+			// appendPoints(this);
+
+			// Scrubber handlers
+			this.attrs.scrubber.mousemove(function(e){
+				scrubToFrame(e, _this);
+			});
+
+			// Control handlers
+			this.attrs.controls.control.mouseenter(function(){
+				startAnimation(_this);
+			}).mouseleave(function(){
+				stopAnimation(_this);
+			});
+		});
+
+		// Basically make everything
+		function init(self) {
+			var scrubber = $(self).find('img.geef-scrubber'),
+				scrubberHeight = scrubber.innerHeight(),
+				frameHeight = scrubber.innerHeight() / (scrubber.innerHeight() / $(self).innerHeight()),
 				frameWidth = scrubber.innerWidth(),
-				framesCount = (scrubber.innerHeight() / frameHeight)-1,
+				framesCount = (scrubber.innerHeight() / frameHeight)-1, //-1 to remove last frame, avoid flashing
 				spacing = frameWidth / framesCount;
-				timeline = $(this).find('.timeline'),
-				control = $(this).find('.control'),
+				timeline = $(self).find('.timeline'),
+				control = $(self).find('.control'),
 				controlIcon = control.find('i.fa');
 
-			// Build an object to use everywhere
-			this.attrs = {
+			var attrs = {
 				t: 0,
 				interval: null,
 				scrubber: scrubber,
@@ -44,41 +76,43 @@
 				}
 			};
 
-			// appendPoints(this);
+			return attrs;
+		}
 
-			// Scrubber handlers
-			this.attrs.scrubber.mousemove(function(e){
-				scrubToFrame(e, _this);
-			});
 
-			// Control handlers
-			this.attrs.controls.control.mouseenter(function(){
-				startAnimation(_this);
-				_this.attrs.controls.controlIcon.removeClass('fa-play').addClass('fa-pause')
-			}).mouseleave(function(){
-				stopAnimation(_this);
-				_this.attrs.controls.controlIcon.removeClass('fa-pause').addClass('fa-play')
-			});
+		// Setting height of .geef-wrapper
+		function setHeight(height) {
+			var ratio = 16 / 9;
+			return height / ratio;
+		}
 
-		});
-
+		// Append timeline and play/pause controller
 		function appendDOM(geef) {	
 			$(geef).append('<div class="timeline"></div>').append('<div class="control"><i class="fa fa-play"></i></div>');
 		}
 
+		//Start the animation
 		function startAnimation(geef) {
-			var attrs = geef.attrs;
+			var attrs = geef.attrs,
+				perc = 0,
+				activeFrame = 1;
+			// console.log('Frameheight:', attrs.frame.height);
+			attrs.controls.controlIcon.removeClass('fa-play').addClass('fa-pause');
+			
 			attrs.interval = setInterval(function(){
-				var perc = (Math.abs(attrs.t) / attrs.scrubberHeight) * 100;
-				attrs.scrubber.css('top', attrs.t + 'px');
+				perc = (Math.abs(attrs.t) / attrs.scrubberHeight) * 100;
 				attrs.t -= attrs.frame.height;
+				attrs.scrubber.css('top', attrs.t + 'px');
+				console.log('Frame:', activeFrame + ' Position:', attrs.t);
 				animateTimeline(geef, perc);
-				if (attrs.t == -attrs.scrubberHeight) {
+				activeFrame++;
+				if (attrs.t <= -attrs.scrubberHeight) {
 					attrs.t = 0;
 				}
 			}, settings.speed);
 		}
 
+		//DEBUG function - used to append segment tiles on image
 		function appendPoints(geef) { 
 			for(var i = 0; i < geef.attrs.framesCount; i++) {
 				$(geef).append('<div class="line" style="width:'+ geef.attrs.spacing +'px; left:'+ i*geef.attrs.spacing +'px"></div>');
@@ -86,21 +120,24 @@
 		} 
 
 		function stopAnimation(geef) {
-			clearInterval(geef.attrs.interval);
-			geef.attrs.interval = null;
+			var attrs = geef.attrs;
+			attrs.controls.controlIcon.removeClass('fa-pause').addClass('fa-play');
+			clearInterval(attrs.interval);
+			attrs.interval = null;
 		}
 
 		function scrubToFrame(e, geef) {
 			var posX = e.pageX - $(geef).offset().left,
 				percentagePosX = (posX * 100) / geef.attrs.frame.width,	
 				activeFrame = Math.ceil((posX / geef.attrs.spacing));
+			console.log(activeFrame);
 
 			if(activeFrame <= geef.attrs.framesCount) {
 				geef.attrs.t = -Math.abs(activeFrame * geef.attrs.frame.height);
 				geef.attrs.scrubber.css('top', geef.attrs.t + 'px');
 				animateTimeline(geef, percentagePosX);	
 			}
-		}
+		} 
 
 		function animateTimeline(geef, width) {
 			geef.attrs.timeline.css('width', Math.ceil(width) + '%');
